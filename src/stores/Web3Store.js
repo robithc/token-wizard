d2ca030891e4d71bc6ad9fb0bd72e33fdd60e2a6
Mariano Aguero <mariano.aguero@altoros.com>
2018-10-16 09:12:14 -0300

    Update DApp loading code considering upcoming breaking change in metaMask

--- src/stores/Web3Store.js
+++ src/stores/Web3Store.js
@@ -40,12 +40,38 @@ class Web3Store {
   }
 
   getWeb3 = cb => {
+    let { ethereum } = window
+
+    if (ethereum) {
+      window.web3 = new Web3(ethereum)
+      try {
+        // Request account access if needed
+        ethereum.enable().then(() => {
+          this.processWeb3(window.web3, cb)
+        })
+      } catch (error) {
+        // User denied account access...
+        logger.log('User denied account', error)
+      }
+    }
+    // Legacy dapp browsers...
+    else if (window.web3) {
+      window.web3 = new Web3(window.web3.currentProvider)
+      // Acccounts always exposed
+      this.processWeb3(window.web3, cb)
+    }
+    // Non-dapp browsers...
+    else {
+      logger.log('Non-Ethereum browser detected. You should consider trying MetaMask!')
+    }
+  }
+
+  processWeb3 = (web3, cb) => {
     let networkID = CrowdsaleConfig.networkID || getNetworkID()
     networkID = Number(networkID)
-    let web3 = window.web3
     if (typeof web3 === 'undefined') {
       // no web3, use fallback
-      logger.error('Please use a web3 browser')
+      logger.log('Please use a web3 browser')
       const devEnvironment = process.env.NODE_ENV === 'development'
       if (devEnvironment) {
         web3 = new Web3(new Web3.providers.HttpProvider('http://localhost:8545'))
@@ -71,18 +97,20 @@ class Web3Store {
             infuraLink = this.getInfuraLink(CHAINS.MAINNET)
             break
         }
-        const httpProvider = new Web3.providers.HttpProvider(infuraLink)
+        let httpProvider = new Web3.providers.HttpProvider(infuraLink)
         web3 = new Web3(httpProvider)
       }
+
+      cb(web3, false)
+      return web3
     } else {
       // window.web3 == web3 most of the time. Don't override the provided,
       // web3, just wrap it in your Web3.
-      web3 = new Web3(web3.currentProvider)
+      let myWeb3 = new Web3(web3.currentProvider)
+
+      cb(myWeb3, false)
+      return myWeb3
     }
-
-    cb(web3, false)
-
-    return web3
   }
 }
 
